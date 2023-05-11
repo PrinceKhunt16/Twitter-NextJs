d83cf429111c475b5184dc003f805a52228fc1e5
Prince <princekhuntdatastructure@gmail.com>
2023-05-12 00:07:58 +0530

    Complate layout of app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
-      
+      <Header label="Home" />
     </div>
   )
 }
